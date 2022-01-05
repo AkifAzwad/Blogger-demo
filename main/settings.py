1e57e5b90af99e7d936e37602d6f197083f34db3
AkifAzwad <sustgeneric@gmail.com>
2022-01-06 02:25:09 +0600

    collect static

--- main/settings.py
+++ main/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
+
     'tinymce',
     'crispy_forms',
-    
+
 
     'posts',
     'marketing',
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_in_env')
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 
 # Default primary key field type
